286f66c305973d5f3a3d0bedec3f8fc0e8a3b871
Francesco Bartoli <francesco.bartoli@geobeyond.it>
2018-04-23 12:28:16 +0200

    Fix init file namespaces

--- pystac/models/__init__.py
+++ pystac/models/__init__.py
@@ -1,6 +1,4 @@
 from .asset import Asset
-from .band import Band
-from .feature import Feature
+from .item import Item
 from .link import Link
-from .product import Product
 from .properties import Properties
