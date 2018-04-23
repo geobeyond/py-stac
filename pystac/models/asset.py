9d9ccdf9666dd41667fdfb51b24e00b6ffc44f0a
Francesco Bartoli <francesco.bartoli@geobeyond.it>
2018-04-23 12:04:22 +0200

    Align asset schema to spec 0.4

--- pystac/models/asset.py
+++ pystac/models/asset.py
@@ -3,17 +3,14 @@ from marshmallow import Schema, fields
 
 
 class Asset(STACObject):
-    def __init__(self, href, file_format, product="", name=""):
-        """Asset referenced by feature
+    def __init__(self, href, name=""):
+        """Asset referenced by item
 
         Args:
             href (str): location (full or relative) of asset
             name (str): human readable name of asset
-            product (str): location of product definition
-            file_format (str): filetype
         """
-        self.format = file_format
-        self.product = product
+
         self.name = name
         self.href = href
 
@@ -25,10 +22,7 @@ class Asset(STACObject):
         )
         if self.name:
             base_properties['name'] = self.name
-        if self.product:
-            base_properties['product'] = self.product
-        if self.format:
-            base_properties['format'] = self.format
+
         return base_properties
 
 
@@ -41,7 +35,5 @@ class Asset(STACObject):
 
 class AssetSchema(Schema):
 
-    format = fields.Str()
-    product = fields.Str()
     name = fields.Str()
     href = fields.Str() #  TBD fields.URL()
