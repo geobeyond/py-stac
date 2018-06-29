f15f3511737c35fca27da21b38a98960e21f4293
Francesco Bartoli <francesco.bartoli@geobeyond.it>
2018-06-29 12:49:25 +0200

    Bump version to 0.2.1

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open('HISTORY.rst') as history_file:
 
 setup(
     name='stacpy',
-    version='0.2.0',
+    version='0.2.1',
     description=(
         "Python command line and library utilities for interacting"
         "with and creating STAC compliant files."
